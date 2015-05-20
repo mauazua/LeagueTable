3396bacfcc028e82822914afceb47dcb44452ebb
Marta Wyszynska <marwyszynska@gmail.com>
2015-05-20 21:16:56 +0200

    add new methods to LeagueTable class
    add get_scores, get_goals_for, get_goals_against methods, needed for
    calculating amount of won or lost goals

--- league_table.rb
+++ league_table.rb
@@ -2,4 +2,26 @@ class LeagueTable < Array
   def find_scores(team)
     self.find_all {|score| score.include?(team)}
   end
+
+  def get_scores(team, gained = true)
+    goals = []
+    find_scores(team).each {|s| goals << s.split("-")}
+    if gained
+      goals.flatten.delete_if {|f| !f.include?(team)}.each {|s| s.strip!}
+    else
+      goals.flatten.delete_if {|f| f.include?(team)}.each {|s| s.strip!}
+    end
+  end
+
+  def get_goals_for(team)
+    points = []
+    get_scores(team).each {|p| points << p.split(' ')}
+    points.flatten.map(&:to_i).reduce(:+)
+  end
+
+  def get_goals_against(team)
+    points = []
+    get_scores(team, false).each {|p| points << p.split(' ')}
+    points.flatten.map(&:to_i).reduce(:+)
+  end
 end
