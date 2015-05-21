3d78098d9e58272e86306ee43b777f9a684b5b6c
Marta Wyszynska <marwyszynska@gmail.com>
2015-05-21 17:08:58 +0200

    extract calculation of points to calculate method

--- league_table.rb
+++ league_table.rb
@@ -13,18 +13,21 @@ class LeagueTable < Array
     end
   end
 
+  def calculate(points)
+    return points.flatten.map(&:to_i).reduce(:+) if points.any?
+    return 0 if points.empty?
+  end
+
   def get_goals_for(team)
     points = []
     get_scores(team).each {|p| points << p.split(' ')}
-    return points.flatten.map(&:to_i).reduce(:+) if points.any?
-    return 0 if points.empty?
+    calculate(points)
   end
 
   def get_goals_against(team)
     points = []
     get_scores(team, false).each {|p| points << p.split(' ')}
-    return points.flatten.map(&:to_i).reduce(:+) if points.any?
-    return 0 if points.empty?
+    calculate(points)
   end
 
   def get_goals_difference(team)
